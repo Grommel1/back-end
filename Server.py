3228bd74cc78584bce4b10b5d553ef4922177681
Grommel1 <florianj1802@gmail.com>
2026-01-17 03:00:58 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify
 import requests
 
-API_KEY = "HIER_DEIN_API_KEY"
-CLAN_TAG = "#DEINCLANTAG"
+API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjcyMTExZGUxLWY2ZGQtNDdlNS1iOGFhLTBmYTQyYTk5NjAyNyIsImlhdCI6MTc2ODYxNDkxMCwic3ViIjoiZGV2ZWxvcGVyLzc1YjQzNDAzLWUzYWEtYTdkOC05MzdlLWU2N2FiZDc2NTJlNyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3NC4yMjAuNTEuMCIsIjc0LjIyMC41OS4wIiwiNzQuMjIwLjU5LjI0IiwiNzQuMjIwLjUxLjI0Il0sInR5cGUiOiJjbGllbnQifV19.Z83OoT-T2jGDaGrkdlDBrdPv-96kX5xrB_xR0cqkhFrNLoAoD_SPy5NprUamxYNmER2lcvZSZtGyKUGTVM8BDA"
+CLAN_TAG = "FEUERDOCTOR.de"
 
 app = Flask(__name__)
 
@@ -18,3 +18,4 @@ def clan():
 
 if __name__ == "__main__":
     app.run()
+
